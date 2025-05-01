70790c8deaba4f481dd769d64a45dec567a9a002
nicolasraffray <nraffray@gmail.com>
2025-05-01 23:36:35 +0100

    test

--- init.lua
+++ init.lua
@@ -86,7 +86,7 @@ P.S. You can delete this when you're done too. It's your config now! :)
 
 -- Set <space> as the leader key
 -- See `:help mapleader`
---  NOTE: Must happen before plugins are loaded (otherwise wrong leader will be used)
+--  NOTE: Must happen before plugins are loaded (otherwise wrong leader will be used).
 vim.g.mapleader = ' '
 vim.g.maplocalleader = ' '
 
