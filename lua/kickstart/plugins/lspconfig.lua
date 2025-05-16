0adec04b0b26bcc136e8f64b5d5eec60e590a2a9
nicolasraffray <nraffray@gmail.com>
2025-05-16 10:49:04 +0100

    ts file formatting lsp

--- lua/kickstart/plugins/lspconfig.lua
+++ lua/kickstart/plugins/lspconfig.lua
@@ -190,6 +190,16 @@ return {
         end,
       })
 
+      vim.api.nvim_create_autocmd('FileType', {
+        pattern = { 'ts', 'typescript', 'typescriptreact', 'javascript', 'javascriptreact' },
+        callback = function()
+          vim.opt_local.expandtab = true
+          vim.opt_local.shiftwidth = 2
+          vim.opt_local.tabstop = 2
+          vim.opt_local.softtabstop = 2
+        end,
+      })
+
       -- Diagnostic Config
       -- See :help vim.diagnostic.Opts
       vim.diagnostic.config {
